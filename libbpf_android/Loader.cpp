b28bf2aa531c28af1ec04da602873783ffafc3a4
Patrick Rohr <prohr@google.com>
2021-10-13 08:26:50 +0200

    bpfloader: add schedact support
    This also bumps the bpfloader version to 0.3 to signify support
    for loading schedact (ie. tc scheduler action) programs.
    
    Test: load netd.o
    Change-Id: Ic3fdc80d3f6ea2b42a4f1d6829b1ff12dd4d52c0

--- libbpf_android/Loader.cpp
+++ libbpf_android/Loader.cpp
@@ -28,9 +28,9 @@
 #include <sys/utsname.h>
 #include <unistd.h>
 
-// This is BpfLoader v0.2
+// This is BpfLoader v0.3
 #define BPFLOADER_VERSION_MAJOR 0u
-#define BPFLOADER_VERSION_MINOR 2u
+#define BPFLOADER_VERSION_MINOR 3u
 #define BPFLOADER_VERSION ((BPFLOADER_VERSION_MAJOR << 16) | BPFLOADER_VERSION_MINOR)
 
 #include "../progs/include/bpf_map_def.h"
@@ -92,6 +92,7 @@ sectionType sectionNameTypes[] = {
         {"skfilter", BPF_PROG_TYPE_SOCKET_FILTER},
         {"cgroupskb", BPF_PROG_TYPE_CGROUP_SKB},
         {"schedcls", BPF_PROG_TYPE_SCHED_CLS},
+        {"schedact", BPF_PROG_TYPE_SCHED_ACT},
         {"cgroupsock", BPF_PROG_TYPE_CGROUP_SOCK},
         {"xdp", BPF_PROG_TYPE_XDP},
 
