21d898fddb36f7d9fb5e9550c44ee9529e192851
Michael Bestas <mkbestas@gmail.com>
2024-03-11 01:52:21 +0200

    bpfloader: Support <4.14 kernels
    Change-Id: I3d6573c102c1d64a64209bf61ecb8ed0c4ecb64c

--- bpfloader/BpfLoader.cpp
+++ bpfloader/BpfLoader.cpp
@@ -197,7 +197,8 @@ int main(int argc, char** argv) {
     // Enable JIT kallsyms export for privileged users only
     // (Note: this (open) will fail with ENOENT 'No such file or directory' if
     //  kernel does not have CONFIG_HAVE_EBPF_JIT=y)
-    if (writeProcSysFile("/proc/sys/net/core/bpf_jit_kallsyms", "1\n")) return 1;
+    if (writeProcSysFile("/proc/sys/net/core/bpf_jit_kallsyms", "1\n") &&
+        android::bpf::isAtLeastKernelVersion(4, 14, 0)) return 1;
 
     // Create all the pin subdirectories
     // (this must be done first to allow selinux_context and pin_subdir functionality,
