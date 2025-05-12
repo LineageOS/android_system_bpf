867009fdabb310e9f720d620d201af6f3e2bfe5c
Motomu Utsumi <motomuman@google.com>
2025-05-12 12:10:18 +0900

    Add DEFINE_BPF_PROG_KVER to android_bpf_defs.h
    Bug: 410982483
    Test: m bpfMemEvents.bpf bpfMemEventsTest.bpf
    Change-Id: I97b7cbe5d033dbe4b15a52b1487e9ace3d12ee7d

--- include/defs/android_bpf_defs.h
+++ include/defs/android_bpf_defs.h
@@ -44,6 +44,9 @@
     SEC(SECTION_NAME)                                               \
     int the_prog
 
+#define DEFINE_BPF_PROG_KVER(SECTION_NAME, prog_uid, prog_gid, the_prog, kver) \
+    DEFINE_BPF_PROG(SECTION_NAME, prog_uid, prog_gid, the_prog)
+
 #define LICENSE(NAME) char _license[] SEC("license") = (NAME)
 
 #else  // LIBBPF DISABLED
