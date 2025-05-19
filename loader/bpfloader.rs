519bf2e7fd16191ab44fe108559ac444bdf5be12
Motomu Utsumi <motomuman@google.com>
2025-05-19 11:08:17 +0900

    Explicitly set SocketFilter type for skfilter/ programs
    This is a no-op for existing timeInState.bpf
    
    Bug: 410982483
    Test: TH
    Change-Id: Ie73a9f99b591ae730c91988ab67e36d2a13729ce

--- loader/bpfloader.rs
+++ loader/bpfloader.rs
@@ -19,7 +19,7 @@
 use android_ids::{AID_ROOT, AID_SYSTEM};
 use android_logger::AndroidLogger;
 use anyhow::{anyhow, ensure};
-use libbpf_rs::{set_print, MapCore, ObjectBuilder, PrintLevel};
+use libbpf_rs::{set_print, MapCore, ObjectBuilder, OpenObject, PrintLevel, ProgramType};
 use libc::{
     mode_t, uname, utsname, S_IRGRP, S_IRUSR, S_IRWXG, S_IRWXO, S_IRWXU, S_ISVTX, S_IWGRP, S_IWUSR,
 };
@@ -221,6 +221,17 @@ const FILE_ARR: &[BpfFileDesc] = &[BpfFileDesc {
     ],
 }];
 
+fn set_prog_types(open_file: &mut OpenObject) -> Result<(), anyhow::Error> {
+    for mut prog in open_file.progs_mut() {
+        let section_name =
+            prog.section().to_str().ok_or_else(|| anyhow!("Failed to parse prog section name"))?;
+        if section_name.starts_with("skfilter/") {
+            prog.set_prog_type(ProgramType::SocketFilter);
+        }
+    }
+    Ok(())
+}
+
 fn create_dir(dir_path: &Path) -> Result<(), anyhow::Error> {
     if dir_path.exists() {
         return Ok(());
@@ -289,7 +300,10 @@ fn libbpf_worker(file_desc: &BpfFileDesc) -> Result<(), anyhow::Error> {
     let filename = filename.to_str().ok_or_else(|| anyhow!("Failed to parse filename"))?;
 
     let mut ob = ObjectBuilder::default();
-    let open_file = ob.open_file(&filepath)?;
+    let mut open_file = ob.open_file(&filepath)?;
+    // libbpf's open_file attempts to infer the prog type based on the section name. But, some
+    // section names are not recognized, so the program type must be set explicitly for them.
+    set_prog_types(&mut open_file)?;
     let mut loaded_file = open_file.load()?;
 
     let bpffs_path = "/sys/fs/bpf/".to_owned() + file_desc.prefix;
